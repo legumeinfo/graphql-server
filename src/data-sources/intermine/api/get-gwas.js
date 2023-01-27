f1f0725306124c01067e796f61d883d4352906b2
Sam Hokin <shokin@ncgr.org>
2023-01-27 14:32:07 -0700

    Resolve Trait.gwas and Trait.qtlStudy.

--- src/data-sources/intermine/api/get-gwas.js
+++ src/data-sources/intermine/api/get-gwas.js
@@ -17,5 +17,25 @@ async function getGWAS(id) {
         });
 }
 
+// get the GWAS for a Trait
+async function getGWAS({trait}={}) {
+    if (trait) {
+        const constraints = [this.pathquery.intermineConstraint('GWAS.results.trait.id', '=', trait.id)];
+        const query = this.pathquery.interminePathQuery(
+            this.models.intermineGWASAttributes,
+            this.models.intermineGWASSort,
+            constraints,
+        );
+        return this.pathQuery(query)
+            .then((response) => this.models.response2gwas(response))
+            .then((gwas) => {
+                if (gwas.length) {
+                    return gwas[0];
+                } else {
+                    return null;
+                }
+            });
+    }
+}
 
 module.exports = getGWAS;
--- src/data-sources/intermine/api/get-qtl-study.js
+++ src/data-sources/intermine/api/get-qtl-study.js
@@ -17,5 +17,26 @@ async function getQTLStudy(id) {
         });
 }
 
+// get the QTLStudy for a Trait
+async function getQTLStudy({trait}={}) {
+    if (trait) {
+        const constraints = [this.pathquery.intermineConstraint('QTLStudy.qtls.trait.id', '=', trait.id)];
+        const query = this.pathquery.interminePathQuery(
+            this.models.intermineQTLStudyAttributes,
+            this.models.intermineQTLStudySort,
+            constraints,
+        );
+        return this.pathQuery(query)
+            .then((response) => this.models.response2qtlStudies(response))
+            .then((qtlStudies) => {
+                if (qtlStudies.length) {
+                    return qtlStudies[0];
+                } else {
+                    return null;
+                }
+            });
+    }
+}
+
 
 module.exports = getQTLStudy;
