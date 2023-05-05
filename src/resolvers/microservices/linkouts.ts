7c189909b546f4c9458fa1af12bb165f656bade0
Alan Cleary <acleary@ncgr.org>
2023-05-05 10:26:21 -0600

    Changed "Linkout" to "Linkouts" in GraphQL types and resolvers.
    This is because more than one linkout can be returned for a single entity so it should be plural.

--- src/resolvers/microservices/linkouts.ts
+++ src/resolvers/microservices/linkouts.ts
@@ -6,10 +6,10 @@ import { ResolverMap } from '../resolver.js';
 export const linkoutsFactory = (sourceName: KeyOfType<DataSources, MicroservicesAPI>):
 ResolverMap => ({
     Query: {
-        geneLinkout: async (_, { identifier }, { dataSources }) => {
+        geneLinkouts: async (_, { identifier }, { dataSources }) => {
             return dataSources[sourceName].getLinkoutsForGene(identifier);
         },
-        locationLinkout: async (_, { identifier, start, end }, { dataSources }) => {
+        locationLinkouts: async (_, { identifier, start, end }, { dataSources }) => {
             return dataSources[sourceName].getLinkoutsForLocation(identifier, start, end);
         },
     },
