50ed1d7fb56ab379e466fb1b4f2ddb0179f269f5
Sam Hokin <shokin@ncgr.org>
2024-01-12 13:21:22 -0700

    Updated QTLStudy.

--- src/data-sources/intermine/models/qtl-study.ts
+++ src/data-sources/intermine/models/qtl-study.ts
@@ -1,62 +1,46 @@
 import { IntermineDataResponse, response2graphqlObjects } from '../intermine.server.js';
 
-
 // <class name="QTLStudy" extends="Annotatable" is-interface="true" term="">
 // 	<attribute name="description" type="java.lang.String"/>
 // 	<attribute name="genotypes" type="java.lang.String"/>
 // 	<attribute name="synopsis" type="java.lang.String"/>
 // 	<reference name="organism" referenced-type="Organism"/>
-// 	<reference name="dataSet" referenced-type="DataSet"/>
 // 	<collection name="qtls" referenced-type="QTL" reverse-reference="qtlStudy"/>
 // </class>
 export const intermineQTLStudyAttributes = [
-    'QTLStudy.id',
-    'QTLStudy.primaryIdentifier',
+    'QTLStudy.id',                  // Annotatable
+    'QTLStudy.primaryIdentifier',   // Annotatable
     'QTLStudy.description',
     'QTLStudy.genotypes',
     'QTLStudy.synopsis',
-    'QTLStudy.organism.taxonId',
-    'QTLStudy.dataSet.name',
+    'QTLStudy.organism.taxonId',    // resolve reference
 ];
 export const intermineQTLStudySort = 'QTLStudy.primaryIdentifier';
+
 export type IntermineQTLStudy = [
-  number,
-  string,
-  string,
-  string,
-  string,
-  number,
-  string,
+  number, // id
+  string, // primaryIdentifier
+  string, // description
+  string, // genotypes
+  string, // synopsis
+  number, // organism.taxonId
 ];
 
-
-// type QTLStudy {
-//   id: ID!
-//   identifier: String!
-//   # ontologyAnnotations
-//   # publications
-//   description: String
-//   genotypes: String
-//   synopsis: String
-//   organism: Organism
-//   dataSet: DataSet
-//   qtls: [QTL]
-// }
 export const graphqlQTLStudyAttributes = [
-    'id',
-    'identifier',
-    'description',
-    'genotypes',
-    'synopsis',
-    'organismTaxonId',
-    'dataSetName',
+    'id',              // id
+    'identifier',      // primaryIdentifier
+    'description',     // description
+    'genotypes',       // genotypes
+    'synopsis',        // synopsis
+    'organismTaxonId', // resolve Organism
 ];
+
 export type GraphQLQTLStudy = {
   [prop in typeof graphqlQTLStudyAttributes[number]]: string;
 }
 
-
 export type IntermineQTLStudyResponse = IntermineDataResponse<IntermineQTLStudy>;
+
 export function response2qtlStudies(response: IntermineQTLStudyResponse): Array<GraphQLQTLStudy> {
     return response2graphqlObjects(response, graphqlQTLStudyAttributes);
 }
