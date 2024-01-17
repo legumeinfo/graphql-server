1aa91de19b085d2b635fce8e04c6ed10843456c3
Sam Hokin <shokin@ncgr.org>
2024-01-17 10:51:41 -0700

    Added IntergenicRegion resolution.

---
+++ src/resolvers/intermine/intergenic-region.ts
@@ -0,0 +1,27 @@
+import { DataSources, IntermineAPI } from '../../data-sources/index.js';
+import { inputError, KeyOfType } from '../../utils/index.js';
+import { ResolverMap } from '../resolver.js';
+import { sequenceFeatureInterfaceFactory } from './sequence-feature-interface.js';
+
+export const intergenicRegionFactory = (sourceName: KeyOfType<DataSources, IntermineAPI>):
+ResolverMap => ({
+    Query: {
+        intergenicRegion: async (_, { identifier }, { dataSources }) => {
+            const {data: intergenicRegion} = await dataSources[sourceName].getIntergenicRegion(identifier);
+            if (intergenicRegion == null) {
+                const msg = `IntergenicRegion with primaryIdentifier '${identifier}' not found`;
+                inputError(msg);
+            }
+            return {results: intergenicRegion};
+        },
+    },
+    IntergenicRegion: {
+        ...sequenceFeatureInterfaceFactory(sourceName),
+        adjacentGenes: async (intergenicRegion, { page, pageSize }, { dataSources }) => {
+            const args = {intergenicRegion, page, pageSize};
+            return dataSources[sourceName].getAdjacentGenes(args)
+            // @ts-ignore: implicit type any error
+                .then(({data: results}) => results);
+        },
+    },
+});
