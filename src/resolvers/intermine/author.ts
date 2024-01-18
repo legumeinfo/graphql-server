0894223ceeb8b0427e9a9aae30daa253a07627e9
Sam Hokin <shokin@ncgr.org>
2024-01-18 14:44:56 -0700

    Updated Author.

--- src/resolvers/intermine/author.ts
+++ src/resolvers/intermine/author.ts
@@ -2,7 +2,8 @@ import { DataSources, IntermineAPI } from '../../data-sources/index.js';
 import { inputError, KeyOfType } from '../../utils/index.js';
 import { ResolverMap } from '../resolver.js';
 
-
+// NOTE: we get Author by id because there is no reliable string identifier. Neither
+// lastName, firstName, nor name are reliable identifiers for a particular author.
 export const authorFactory = (sourceName: KeyOfType<DataSources, IntermineAPI>):
 ResolverMap => ({
     Query: {
