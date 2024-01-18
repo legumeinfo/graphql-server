ffcd2a5a500b406c3397c64c2bb42f7952d8f464
Sam Hokin <shokin@ncgr.org>
2024-01-17 17:28:42 -0700

    Updated SyntenicRegion.

--- src/data-sources/intermine/api/get-syntenic-region.ts
+++ src/data-sources/intermine/api/get-syntenic-region.ts
@@ -1,25 +1,34 @@
 import {
-  ApiResponse,
-  intermineConstraint,
-  interminePathQuery,
+    ApiResponse,
+    intermineConstraint,
+    intermineJoin,
+    interminePathQuery,
 } from '../intermine.server.js';
 import {
-  GraphQLSyntenicRegion,
-  IntermineSyntenicRegionResponse,
-  intermineSyntenicRegionAttributes,
-  intermineSyntenicRegionSort,
-  response2syntenicRegions,
+    GraphQLSyntenicRegion,
+    IntermineSyntenicRegionResponse,
+    intermineSyntenicRegionAttributes,
+    intermineSyntenicRegionSort,
+    response2syntenicRegions,
 } from '../models/index.js';
-
 
 // get a SyntenicRegion by identifier
 export async function getSyntenicRegion(identifier: string):
 Promise<ApiResponse<GraphQLSyntenicRegion>> {
     const constraints = [intermineConstraint('SyntenicRegion.primaryIdentifier', '=', identifier)];
+    // all SequenceFeature-extending object queries must include these joins
+    const joins = [
+        intermineJoin('SyntenicRegion.chromosome', 'OUTER'),
+        intermineJoin('SyntenicRegion.supercontig', 'OUTER'),
+        intermineJoin('SyntenicRegion.chromosomeLocation', 'OUTER'),
+        intermineJoin('SyntenicRegion.supercontigLocation', 'OUTER'),
+        intermineJoin('SyntenicRegion.sequenceOntologyTerm', 'OUTER')
+    ];
     const query = interminePathQuery(
         intermineSyntenicRegionAttributes,
         intermineSyntenicRegionSort,
         constraints,
+        joins,
     );
     return this.pathQuery(query)
         .then((response: IntermineSyntenicRegionResponse) => response2syntenicRegions(response))
--- src/data-sources/intermine/models/syntenic-region.ts
+++ src/data-sources/intermine/models/syntenic-region.ts
@@ -1,87 +1,49 @@
 import { IntermineDataResponse, response2graphqlObjects } from '../intermine.server.js';
-
+import { IntermineSequenceFeature, graphqlSequenceFeatureAttributes } from './sequence-feature.js';
 
 // <class name="SyntenicRegion" extends="SequenceFeature" is-interface="true" term="http://purl.obolibrary.org/obo/SO_0005858">
 // 	<reference name="syntenyBlock" referenced-type="SyntenyBlock" reverse-reference="syntenicRegions"/>
 // </class>
 export const intermineSyntenicRegionAttributes = [
     'SyntenicRegion.id',
-    'SyntenicRegion.primaryIdentifier',
-    'SyntenicRegion.description',
-    'SyntenicRegion.symbol',
-    'SyntenicRegion.name',
-    'SyntenicRegion.assemblyVersion',
-    'SyntenicRegion.annotationVersion',
-    'SyntenicRegion.organism.taxonId',
-    'SyntenicRegion.strain.identifier',
-    'SyntenicRegion.length',
-    'SyntenicRegion.syntenyBlock.id',
+    'SyntenicRegion.primaryIdentifier',               // Annotatable
+    'SyntenicRegion.description',                     // BioEntity
+    'SyntenicRegion.symbol',                          // BioEntity
+    'SyntenicRegion.name',                            // BioEntity
+    'SyntenicRegion.assemblyVersion',                 // BioEntity
+    'SyntenicRegion.annotationVersion',               // BioEntity
+    'SyntenicRegion.secondaryIdentifier',             // BioEntity
+    'SyntenicRegion.organism.taxonId',                // BioEntity - reference resolution
+    'SyntenicRegion.strain.identifier',               // BioEntity - reference resolution
+    'SyntenicRegion.score',                           // SequenceFeature
+    'SyntenicRegion.scoreType',                       // SequenceFeature
+    'SyntenicRegion.length',                          // SequenceFeature
+    'SyntenicRegion.sequenceOntologyTerm.identifier', // SequenceFeature - reference resolution
+    'SyntenicRegion.chromosomeLocation.id',           // SequenceFeature - reference resolution
+    'SyntenicRegion.supercontigLocation.id',          // SequenceFeature - reference resolution
+    'SyntenicRegion.sequence.id',                     // SequenceFeature - reference resolution
+    'SyntenicRegion.chromosome.primaryIdentifier',    // SequenceFeature - reference resolution
+    'SyntenicRegion.supercontig.primaryIdentifier',   // SequenceFeature - reference resolution
+    'SyntenicRegion.syntenyBlock.id',                 // resolve reference
 ];
 export const intermineSyntenicRegionSort = 'SyntenicRegion.primaryIdentifier';
+
 export type IntermineSyntenicRegion = [
-  number,
-  string,
-  string,
-  string,
-  string,
-  string,
-  string,
-  number,
-  string,
-  number,
-  number,
+    ...IntermineSequenceFeature,
+    number, // syntenyBlock.id
 ];
 
-
-// type SyntenicRegion implements SequenceFeature {
-//   id: ID!
-//   identifier: String!
-//   ontologyAnnotations: [OntologyAnnotation]
-//   publications: [Publication]
-//   description: String
-//   symbol: String
-//   name: String
-//   assemblyVersion: String
-//   annotationVersion: String
-//   organism: Organism
-//   strain: Strain
-//   locations: [Location]
-//   # synonyms
-//   # crossReferences
-//   # dataSets
-//   # locatedFeatures
-//   # score
-//   # scoreType
-//   length: Int
-//   # sequenceOntologyTerm
-//   # supercontigLocation
-//   # chromosomeLocation
-//   # supercontig
-//   # sequence
-//   # chromosome
-//   # overlappingFeatures
-//   # childFeatures
-//   syntenyBlock: [SyntenyBlock]
-// }
 export const graphqlSyntenicRegionAttributes = [
-    'id',
-    'identifier',
-    'description',
-    'symbol',
-    'name',
-    'assemblyVersion',
-    'annotationVersion',
-    'organismTaxonId',
-    'strainIdentifier',
-    'length',
-    'syntenyBlockId',
+    ...graphqlSequenceFeatureAttributes,
+    'syntenyBlockId', // syntenyBlock.id
 ];
+
 export type GraphQLSyntenicRegion = {
   [prop in typeof graphqlSyntenicRegionAttributes[number]]: string;
 }
-    
 
 export type IntermineSyntenicRegionResponse = IntermineDataResponse<IntermineSyntenicRegion>;
+
 // converts an Intermine response into an array of GraphQL SyntenicRegion objects
 export function response2syntenicRegions(response: IntermineSyntenicRegionResponse): Array<GraphQLSyntenicRegion> {
     return response2graphqlObjects(response, graphqlSyntenicRegionAttributes);
