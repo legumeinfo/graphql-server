af79fb6f603ecf4d5a5a4e8ce453dc90b5392552
alancleary <acleary@ncgr.org>
2024-03-27 11:57:21 -0600

    Annotatable now implements HasDataSets
    The Annotatable resolver was updated accordingly and all types that (transitively) implement Annotatable were updated as well.

--- src/resolvers/intermine/annotatable.ts
+++ src/resolvers/intermine/annotatable.ts
@@ -1,10 +1,12 @@
 import { DataSources, IntermineAPI } from '../../data-sources/index.js';
 import { KeyOfType } from '../../utils/index.js';
 import { SubfieldResolverMap } from '../resolver.js';
+import { hasDataSetsFactory } from './data-set.js';
 
 
 export const annotatableFactory =
 (sourceName: KeyOfType<DataSources, IntermineAPI>): SubfieldResolverMap => ({
+    ...hasDataSetsFactory(sourceName),
     ontologyAnnotations: async (annotatable, { page, pageSize }, { dataSources }) => {
         const args = {annotatable, page, pageSize};
         return dataSources[sourceName].getOntologyAnnotations(args)
