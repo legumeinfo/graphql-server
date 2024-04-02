d359495de47b5331e9ea29cf4bd71c01162e33fe
alancleary <acleary@ncgr.org>
2024-04-02 14:58:15 -0600

    Updated Annotatable data source model

--- src/data-sources/intermine/models/annotatable.ts
+++ src/data-sources/intermine/models/annotatable.ts
@@ -1,48 +1,50 @@
 import { GraphQLBioEntity } from './bio-entity.js';
-import { GraphQLExpressionSource } from './expression-source.js';
 import { GraphQLExpressionSample } from './expression-sample.js';
+import { GraphQLExpressionSource } from './expression-source.js';
 import { GraphQLGeneFamily } from './gene-family.js';
 import { GraphQLGeneticMap } from './genetic-map.js';
 import { GraphQLGenotypingPlatform } from './genotyping-platform.js';
 import { GraphQLGWAS } from './gwas.js';
+import { GraphQLGWASResult } from './gwas-result.js';
 import { GraphQLLinkageGroup } from './linkage-group.js';
 import { GraphQLPanGeneSet } from './pan-gene-set.js';
 import { GraphQLPathway } from './pathway.js';
 import { GraphQLPhylotree } from './phylotree.js';
 import { GraphQLProteinDomain } from './protein-domain.js';
-import { GraphQLTrait } from './trait.js';
 import { GraphQLQTL } from './qtl.js';
 import { GraphQLQTLStudy } from './qtl-study.js';
+import { GraphQLTrait } from './trait.js';
 
 
 export type GraphQLAnnotatable =
-    GraphQLBioEntity |  // all BioEntities are Annotatable
-    GraphQLExpressionSource |
+    GraphQLBioEntity |
     GraphQLExpressionSample |
+    GraphQLExpressionSource |
     GraphQLGeneFamily |
     GraphQLGeneticMap |
     GraphQLGenotypingPlatform |
     GraphQLGWAS |
+    GraphQLGWASResult |
     GraphQLLinkageGroup |
     GraphQLPanGeneSet |
     GraphQLPathway |
     GraphQLPhylotree |
     GraphQLProteinDomain |
-    GraphQLTrait |
     GraphQLQTL |
-    GraphQLQTLStudy;
+    GraphQLQTLStudy |
+    GraphQLTrait;
 
 export const intermineAnnotatableAttributesFactory = (type = 'Annotatable') => [
     `${type}.id`,
-    `${type}.primaryIdentifier`,  // Annotatable
+    `${type}.primaryIdentifier`,
 ];
 
 export type IntermineAnnotatable = [
-    number, // id
-    string, // primaryIdentifier
+    number,
+    string,
 ];
 
 export const graphqlAnnotatableAttributes = [
-    'id',         // id
-    'identifier', // primaryIdentifier
+    'id',
+    'identifier',
 ];
