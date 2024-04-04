9e46f569fb6d01023e052576380e9d5ee3804874
alancleary <acleary@ncgr.org>
2024-04-04 09:08:43 -0600

    Removed InterMine comments from Author model

--- src/data-sources/intermine/models/author.ts
+++ src/data-sources/intermine/models/author.ts
@@ -1,13 +1,5 @@
 import { IntermineDataResponse, response2graphqlObjects } from '../intermine.server.js';
 
-
-// <class name="Author" is-interface="true" term="http://purl.obolibrary.org/obo/NCIT_C42781">
-// 	<attribute name="firstName" type="java.lang.String" term="http://xmlns.com/foaf/0.1/givenname"/>
-// 	<attribute name="initials" type="java.lang.String"/>
-// 	<attribute name="lastName" type="java.lang.String" term="http://xmlns.com/foaf/0.1/family_name"/>
-// 	<attribute name="name" type="java.lang.String" term="http://xmlns.com/foaf/0.1/name"/>
-// 	<collection name="publications" referenced-type="Publication" reverse-reference="authors"/>
-// </class>
 export const intermineAuthorAttributes = [
     'Author.id',
     'Author.firstName',
@@ -24,15 +16,6 @@ export type IntermineAuthor = [
   string,
 ];
 
-
-// type Author {
-//   id: ID!
-//   firstName: String
-//   initials: String
-//   lastName: String
-//   name: String
-//   publications: [Publication]
-// }
 export const graphqlAuthorAttributes = [
     'id',
     'firstName',
