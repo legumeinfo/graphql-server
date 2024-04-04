d7eb75be34fb95d21b02b646fdb5858763dca6ce
alancleary <acleary@ncgr.org>
2024-04-04 14:23:38 -0600

    Cosmetic updates to ExpressionSource

--- src/data-sources/intermine/models/expression-source.ts
+++ src/data-sources/intermine/models/expression-source.ts
@@ -5,19 +5,6 @@ import {
   intermineAnnotatableAttributesFactory,
 } from './annotatable.js';
 
-
-// <class name="ExpressionSource" extends="Annotatable" is-interface="true" term="">
-// 	<attribute name="sra" type="java.lang.String"/>
-// 	<attribute name="description" type="java.lang.String"/>
-// 	<attribute name="bioProject" type="java.lang.String"/>
-// 	<attribute name="unit" type="java.lang.String"/>
-// 	<attribute name="geoSeries" type="java.lang.String"/>
-// 	<attribute name="synopsis" type="java.lang.String"/>
-// 	<reference name="organism" referenced-type="Organism"/>
-// 	<reference name="strain" referenced-type="Strain"/>
-// 	<reference name="dataSet" referenced-type="DataSet"/>
-// 	<collection name="samples" referenced-type="ExpressionSample" reverse-reference="source"/>
-// </class>
 export const intermineExpressionSourceAttributes = [
     ...intermineAnnotatableAttributesFactory('ExpressionSource'),
     'ExpressionSource.sra',
@@ -28,7 +15,6 @@ export const intermineExpressionSourceAttributes = [
     'ExpressionSource.synopsis',
     'ExpressionSource.organism.taxonId',
     'ExpressionSource.strain.identifier',
-    'ExpressionSource.dataSet.name',
 ];
 export const intermineExpressionSourceSort = 'ExpressionSource.primaryIdentifier';
 export type IntermineExpressionSource = [
@@ -41,26 +27,8 @@ export type IntermineExpressionSource = [
   string,
   number,
   string,
-  string,
 ];
 
-
-// type ExpressionSource {
-//   id: ID!
-//   identifier: String!
-//   # ontologyAnnotations
-//   # publications
-//   sra: String
-//   description: String
-//   bioProject: String
-//   unit: String
-//   geoSeries: String
-//   synopsis: String
-//   organism: Organism
-//   strain: Strain
-//   dataSet: DataSet
-//   samples: [ExpressionSample]
-// }
 export const graphqlExpressionSourceAttributes = [
     ...graphqlAnnotatableAttributes,
     'sra',
@@ -71,12 +39,10 @@ export const graphqlExpressionSourceAttributes = [
     'synopsis',
     'organismTaxonId',
     'strainIdentifier',
-    'dataSetName',
 ];
 export type GraphQLExpressionSource = {
   [prop in typeof graphqlExpressionSourceAttributes[number]]: string;
 }
-
 
 export type IntermineExpressionSourceResponse = IntermineDataResponse<IntermineExpressionSource>;
 export function response2expressionSources(response: IntermineExpressionSourceResponse): Array<GraphQLExpressionSource> {
