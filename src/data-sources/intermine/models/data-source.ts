0f508a7f6504832043f285d0c173650c7e465796
alancleary <acleary@ncgr.org>
2024-04-04 09:18:17 -0600

    Removed InterMine comments from DataSource model

--- src/data-sources/intermine/models/data-source.ts
+++ src/data-sources/intermine/models/data-source.ts
@@ -1,12 +1,5 @@
 import { IntermineDataResponse, response2graphqlObjects } from '../intermine.server.js';
 
-// <class name="DataSource" is-interface="true" term="http://purl.obolibrary.org/obo/NCIT_C25683">
-// 	<attribute name="description" type="java.lang.String" term="http://purl.org/dc/terms/description"/>
-// 	<attribute name="url" type="java.lang.String" term="http://edamontology.org/data_1052"/>
-// 	<attribute name="name" type="java.lang.String" term="http://www.w3.org/2000/01/rdf-schema#label"/>
-// 	<collection name="publications" referenced-type="Publication" term="http://purl.org/dc/terms/bibliographicCitation"/>
-// 	<collection name="dataSets" referenced-type="DataSet" reverse-reference="dataSource"/>
-// </class>
 export const intermineDataSourceAttributes = [
     'DataSource.id',
     'DataSource.description',
@@ -16,17 +9,17 @@ export const intermineDataSourceAttributes = [
 export const intermineDataSourceSort = 'DataSource.name';
 
 export type IntermineDataSource = [
-    number, // id
-    string, // description
-    string, // url
-    string, // name
+    number,
+    string,
+    string,
+    string,
 ];
 
 export const graphqlDataSourceAttributes = [
-    'id',             // id
-    'description',    // description
-    'url',            // url
-    'name',           // name
+    'id',
+    'description',
+    'url',
+    'name',
 ];
 
 export type GraphQLDataSource = {
