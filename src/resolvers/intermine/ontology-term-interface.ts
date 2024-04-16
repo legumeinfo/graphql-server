31059eb8e4ba5cbc04b536826398146faeb3554d
alancleary <acleary@ncgr.org>
2024-04-16 13:32:04 -0600

    Removed hasOntologyFactory subfield resolver

--- src/resolvers/intermine/ontology-term-interface.ts
+++ src/resolvers/intermine/ontology-term-interface.ts
@@ -2,14 +2,18 @@ import { DataSources, IntermineAPI } from '../../data-sources/index.js';
 import { KeyOfType } from '../../utils/index.js';
 import { SubfieldResolverMap } from '../resolver.js';
 import { hasDataSetsFactory } from './data-set.js';
-import { hasOntologyFactory } from './ontology.js';
 import { hasOntologyAnnotationsFactory } from './ontology-annotation.js';
 import { hasOntologyRelationsFactory } from './ontology-relation.js';
 
 export const isOntologyTermFactory = (sourceName: KeyOfType<DataSources, IntermineAPI>):
 SubfieldResolverMap => ({
     ...hasDataSetsFactory(sourceName),
-    ...hasOntologyFactory(sourceName),
     ...hasOntologyAnnotationsFactory(sourceName),
     ...hasOntologyRelationsFactory(sourceName),
+    ontology: async (parent, _, { dataSources }) => {
+        const {ontologyName} = parent;
+        return dataSources[sourceName].getOntology(ontologyName)
+            // @ts-ignore: implicit type any error
+            .then(({data: results}) => results);
+    },
 });
