bbf21bae8756f104fb1802caa4775cce10773bb1
Alan Cleary <acleary@ncgr.org>
2024-10-17 16:08:19 -0600

    Updated the intermine data source to use POST instead of GET requests
    This prevents URLs querystrings from being longer than the Intermine server supports.

--- src/data-sources/intermine/intermine.server.ts
+++ src/data-sources/intermine/intermine.server.ts
@@ -45,13 +45,22 @@ export class IntermineServer extends RESTDataSource {
     }
 
     async pathQuery(query: string, options={}, format='json', summaryPath:string|undefined=undefined) {
-        const params = {
-            query,
+        const body = {
+            query: query,
             ...this.convertPaginationOptions(options),
             format,
-            summaryPath,
         };
-        return await this.get('query/results', {params});
+        if (summaryPath !== undefined) {
+            body['summaryPath'] = summaryPath;
+        }
+        const request = {
+          body: new URLSearchParams(body).toString(),
+          headers: {
+            'Accept': `application/json;type=${format}`,
+            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+          },
+        };
+        return await this.post('query/results', request);
     }
 
     async pathQueryCount(query: string, options={}) {
