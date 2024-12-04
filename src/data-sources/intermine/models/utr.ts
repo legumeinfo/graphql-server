41b2e827139e12b512e483e5f706ff61c5acc6de
Alan Cleary <acleary@ncgr.org>
2024-12-04 14:17:44 -0700

    Updated UTR model in the InterMine data source to use the intermineSequenceFeatureAttributesFactory
    This allows inherited sequence feature attributes to be changed without requiring manually updating the attributes in the UTR model.

--- src/data-sources/intermine/models/utr.ts
+++ src/data-sources/intermine/models/utr.ts
@@ -1,29 +1,12 @@
 import { IntermineDataResponse, response2graphqlObjects } from '../intermine.server.js';
-import { IntermineSequenceFeature, graphqlSequenceFeatureAttributes } from './sequence-feature.js';
+import {
+    IntermineSequenceFeature,
+    graphqlSequenceFeatureAttributes,
+    intermineSequenceFeatureAttributesFactory,
+} from './sequence-feature.js';
 
-// <class name="UTR" extends="SequenceFeature" is-interface="true" term="http://purl.obolibrary.org/obo/SO_0000316,http://purl.obolibrary.org/obo/SO:0000316">
-// 	<collection name="transcripts" referenced-type="Transcript" reverse-reference="UTRs"/>
-// </class>
 export const intermineUTRAttributes = [
-    'UTR.id',
-    'UTR.primaryIdentifier',               // Annotatable
-    'UTR.description',                     // BioEntity
-    'UTR.symbol',                          // BioEntity
-    'UTR.name',                            // BioEntity
-    'UTR.assemblyVersion',                 // BioEntity
-    'UTR.annotationVersion',               // BioEntity
-    'UTR.secondaryIdentifier',             // BioEntity
-    'UTR.organism.taxonId',                // BioEntity - reference resolution
-    'UTR.strain.identifier',               // BioEntity - reference resolution
-    'UTR.score',                           // SequenceFeature
-    'UTR.scoreType',                       // SequenceFeature
-    'UTR.length',                          // SequenceFeature
-    'UTR.sequenceOntologyTerm.identifier', // SequenceFeature - reference resolution
-    'UTR.chromosomeLocation.id',           // SequenceFeature - reference resolution
-    'UTR.supercontigLocation.id',          // SequenceFeature - reference resolution
-    'UTR.sequence.id',                     // SequenceFeature - reference resolution
-    'UTR.chromosome.primaryIdentifier',    // SequenceFeature - reference resolution
-    'UTR.supercontig.primaryIdentifier',   // SequenceFeature - reference resolution
+    ...intermineSequenceFeatureAttributesFactory('UTR'),
 ];
 export const intermineUTRSort = 'UTR.primaryIdentifier';
 
