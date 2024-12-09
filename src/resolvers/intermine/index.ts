0b9a8ab3e513ca73944860fc515a23e98569e7d5
Alan Cleary <acleary@ncgr.org>
2024-12-09 13:15:14 -0700

    Added resolvers for new types to resolver index and updated GraphQL API

--- src/resolvers/intermine/index.ts
+++ src/resolvers/intermine/index.ts
@@ -6,8 +6,11 @@ import { DataSources, IntermineAPI, MicroservicesAPI } from '../../data-sources/
 import { KeyOfType } from '../../utils/index.js';
 
 import { authorFactory } from './author.js';
+import { cdsFactory } from './cds.js';
 import { chromosomeFactory } from './chromosome.js';
 import { dataSetFactory } from './data-set.js';
+import { dataSourceFactory } from './data-source.js';
+import { exonFactory } from './exon.js';
 import { expressionSampleFactory } from './expression-sample.js';
 import { expressionSourceFactory } from './expression-source.js';
 import { expressionValueFactory } from './expression-value.js';
@@ -15,11 +18,14 @@ import { geneFactory } from './gene.js';
 import { geneFamilyFactory } from './gene-family.js';
 import { geneFamilyAssignmentFactory } from './gene-family-assignment.js';
 import { geneFamilyTallyFactory } from './gene-family-tally.js';
+import { geneFlankingRegionFactory } from './gene-flanking-region.js';
 import { geneticMapFactory } from './genetic-map.js';
 import { geneticMarkerFactory } from './genetic-marker.js';
 import { genotypingPlatformFactory } from './genotyping-platform.js';
 import { gwasFactory } from './gwas.js';
 import { gwasResultFactory } from './gwas-result.js';
+import { intergenicRegionFactory } from './intergenic-region.js';
+import { intronFactory } from './intron.js';
 import { linkageGroupFactory } from './linkage-group.js';
 import { linkageGroupPositionFactory } from './linkage-group-position.js';
 import { locationFactory } from './location.js';
@@ -40,18 +46,23 @@ import { proteinMatchFactory } from './protein-match.js';
 import { publicationFactory } from './publication.js';
 import { qtlFactory } from './qtl.js';
 import { qtlStudyFactory } from './qtl-study.js';
+import { sequenceFactory } from './sequence.js';
 import { sequenceOntologyTermFactory } from './sequence-ontology-term.js';
 import { supercontigFactory } from './supercontig.js';
 import { syntenicRegionFactory } from './syntenic-region.js';
 import { syntenyBlockFactory } from './synteny-block.js';
 import { strainFactory } from './strain.js';
 import { traitFactory } from './trait.js';
+import { utrFactory } from './utr.js';
 
 
 const factories = [
     authorFactory,
+    cdsFactory,
     chromosomeFactory,
     dataSetFactory,
+    dataSourceFactory,
+    exonFactory,
     expressionSampleFactory,
     expressionSourceFactory,
     expressionValueFactory,
@@ -59,11 +70,14 @@ const factories = [
     geneFamilyFactory,
     geneFamilyAssignmentFactory,
     geneFamilyTallyFactory,
+    geneFlankingRegionFactory,
     geneticMapFactory,
     geneticMarkerFactory,
     genotypingPlatformFactory,
     gwasFactory,
     gwasResultFactory,
+    intergenicRegionFactory,
+    intronFactory,
     linkageGroupFactory,
     linkageGroupPositionFactory,
     locationFactory,
@@ -84,12 +98,14 @@ const factories = [
     publicationFactory,
     qtlFactory,
     qtlStudyFactory,
+    sequenceFactory,
     sequenceOntologyTermFactory,
     strainFactory,
     supercontigFactory,
     syntenicRegionFactory,
     syntenyBlockFactory,
     traitFactory,
+    utrFactory,
 ];
 
 
