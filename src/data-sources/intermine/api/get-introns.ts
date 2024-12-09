bd6d6e922bfbd79634584101f4a03c2defe503af
Alan Cleary <acleary@ncgr.org>
2024-12-09 15:11:25 -0700

    Updated getIntrons API in InterMine data source to use generic internal method

--- src/data-sources/intermine/api/get-introns.ts
+++ src/data-sources/intermine/api/get-introns.ts
@@ -15,7 +15,20 @@ import {
 import { PaginationOptions } from './pagination.js';
 import { sequenceFeatureJoinFactory } from './sequence-feature.js';
 
-// Get Introns associated with a Gene
+// get Introns using the given query and returns the expected GraphQL types
+async function getIntrons(pathQuery: string, { page, pageSize }: PaginationOptions): Promise<ApiResponse<GraphQLIntron>> {
+    // get the data
+    const dataPromise = this.pathQuery(pathQuery, {page, pageSize})
+        .then((response: IntermineIntronResponse) => response2introns(response));
+    // get a summary of the data and convert it to page info
+    const pageInfoPromise = this.pathQuery(pathQuery, {summaryPath: 'Intron.id'})
+        .then((response: IntermineSummaryResponse) => response2graphqlPageInfo(response, page, pageSize));
+    // return the expected GraphQL type
+    return Promise.all([dataPromise, pageInfoPromise])
+        .then(([data, pageInfo]) => ({data, metadata: {pageInfo}}));
+}
+
+// get Introns associated with a Gene
 export async function getIntronsForGene(id: number, { page, pageSize }: PaginationOptions): Promise<ApiResponse<GraphQLIntron>> {
     const constraints = [intermineConstraint('Intron.genes.id', '=', id)];
     const joins = sequenceFeatureJoinFactory('Intron');
@@ -25,18 +38,10 @@ export async function getIntronsForGene(id: number, { page, pageSize }: Paginati
         constraints,
         joins,
     );
-    // get the data
-    const dataPromise = this.pathQuery(query, {page, pageSize})
-        .then((response: IntermineIntronResponse) => response2introns(response));
-    // get a summary of the data and convert it to page info
-    const pageInfoPromise = this.pathQuery(query, {summaryPath: 'Intron.id'})
-        .then((response: IntermineSummaryResponse) => response2graphqlPageInfo(response, page, pageSize));
-    // return the expected GraphQL type
-    return Promise.all([dataPromise, pageInfoPromise])
-        .then(([data, pageInfo]) => ({data, metadata: {pageInfo}}));
+    return getIntrons.call(this, query, {page, pageSize});
 }
 
-// Get Introns associated with a Transcript
+// get Introns associated with a Transcript
 export async function getIntronsForTranscript(id: number, { page, pageSize }: PaginationOptions): Promise<ApiResponse<GraphQLIntron>> {
     const constraints = [intermineConstraint('Intron.transcripts.id', '=', id)];
     const joins = sequenceFeatureJoinFactory('Intron');
@@ -46,13 +51,5 @@ export async function getIntronsForTranscript(id: number, { page, pageSize }: Pa
         constraints,
         joins,
     );
-    // get the data
-    const dataPromise = this.pathQuery(query, {page, pageSize})
-        .then((response: IntermineIntronResponse) => response2introns(response));
-    // get a summary of the data and convert it to page info
-    const pageInfoPromise = this.pathQuery(query, {summaryPath: 'Intron.id'})
-        .then((response: IntermineSummaryResponse) => response2graphqlPageInfo(response, page, pageSize));
-    // return the expected GraphQL type
-    return Promise.all([dataPromise, pageInfoPromise])
-        .then(([data, pageInfo]) => ({data, metadata: {pageInfo}}));
+    return getIntrons.call(this, query, {page, pageSize});
 }
