5ab61c3e2920f798e613dbbd5dc0c56bd8661008
Alan Cleary <acleary@ncgr.org>
2024-12-09 14:09:08 -0700

    Removed OntologyRelation entrypoint
    It doesn't have a field that could be used for querying other than ID, which is internal. Realistically this type will only be accessed via related types anyway.

--- src/resolvers/intermine/ontology-relation.ts
+++ src/resolvers/intermine/ontology-relation.ts
@@ -1,18 +1,18 @@
 import { DataSources, IntermineAPI } from '../../data-sources/index.js';
-import { inputError, KeyOfType } from '../../utils/index.js';
+import { KeyOfType } from '../../utils/index.js';
 import { ResolverMap } from '../resolver.js';
 
 // OntologyRelation does not have a string identifier
-export const ontologyRelationFactory = (sourceName: KeyOfType<DataSources, IntermineAPI>):
+export const ontologyRelationFactory = (_: KeyOfType<DataSources, IntermineAPI>):
 ResolverMap => ({
     Query: {
-        ontologyRelation: async (_, { id }, { dataSources }) => {
-            const {data: ontologyRelation} = await dataSources[sourceName].getOntologyRelation(id);
-            if (ontologyRelation == null) {
-                const msg = `OntologyRelation with id '${id}' not found`;
-                inputError(msg);
-            }
-            return {results: ontologyRelation};
-        },
+        //ontologyRelation: async (_, { id }, { dataSources }) => {
+        //    const {data: ontologyRelation} = await dataSources[sourceName].getOntologyRelation(id);
+        //    if (ontologyRelation == null) {
+        //        const msg = `OntologyRelation with id '${id}' not found`;
+        //        inputError(msg);
+        //    }
+        //    return {results: ontologyRelation};
+        //},
     },
 });
