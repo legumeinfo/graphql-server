6e7983888cde3f6e0fb8213d6e98f4b1140853e5
Alan Cleary <acleary@ncgr.org>
2025-02-05 10:03:39 -0700

    Added chromosomes entrypoint to API
    The code supporting this entrypoint already existed but the entrypoint had been removed for some reason. As such, the only changes necessary in the supporting code were to add the appropriate resolver and fix a bug in the data-source API, i.e. the chromosome model needed to be joined with the sequence feature model that it extends.

--- src/resolvers/intermine/chromosome.ts
+++ src/resolvers/intermine/chromosome.ts
@@ -15,6 +15,12 @@ ResolverMap => ({
             }
             return {results: chromosome};
         },
+        chromosomes: async (_, { genus, species, strain, assembly, annotation, page, pageSize }, { dataSources }) => {
+            const args = {genus, species, strain, assembly, annotation, page, pageSize};
+            return dataSources[sourceName].getChromosomes(args)
+                // @ts-ignore: implicit type any error
+                .then(({data: results, metadata: {pageInfo}}) => ({results, pageInfo}));
+        },
     },
     Chromosome: {
         ...isSequenceFeatureFactory(sourceName),
