61838d928f73c0a290a1b5840879e3ddc791b82a
Alan Cleary <acleary@ncgr.org>
2025-04-03 15:06:50 -0600

    Reimplemented newick property of Phylotree

--- src/data-sources/intermine/models/newick.ts
+++ src/data-sources/intermine/models/newick.ts
@@ -1,31 +1,31 @@
 import { IntermineDataResponse, response2graphqlObjects } from '../intermine.server.js';
 
 export const intermineNewickAttributes = [
-    'Newick.identifier',
-    'Newick.contents',
-    'Newick.phylotree.primaryIdentifier',
-    'Newick.geneFamily.primaryIdentifier',
+  'Newick.identifier',
+  'Newick.contents',
+  'Newick.phylotree.primaryIdentifier',
+  'Newick.geneFamily.primaryIdentifier',
 ];
 export const intermineNewickSort = 'Newick.identifier';
 export type IntermineNewick = [
-    string,
-    string,
-    number,
-    number,
+  string,
+  string,
+  number,
+  number,
 ];
 
 export const graphqlNewickAttributes = [
-    'identifier',
-    'contents',
-    'phylotreeIdentifier',
-    'geneFamilyIdentifier',
+  'identifier',
+  'contents',
+  'phylotreeIdentifier',
+  'geneFamilyIdentifier',
 ];
 export type GraphQLNewick = {
-    [prop in typeof graphqlNewickAttributes[number]]: string;
+  [prop in typeof graphqlNewickAttributes[number]]: string;
 }
 
 
 export type IntermineNewickResponse = IntermineDataResponse<IntermineNewick>;
 export function response2newicks(response: IntermineNewickResponse): Array<GraphQLNewick> {
-    return response2graphqlObjects(response, graphqlNewickAttributes);
+  return response2graphqlObjects(response, graphqlNewickAttributes);
 }
