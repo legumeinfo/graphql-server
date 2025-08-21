e4eba4d40c4da2558f7253989d2e7c7a116baaae
Matt Wiese <github@mattwie.se>
2025-08-21 15:28:12 -0400

    Add inner join for strain

--- src/data-sources/intermine/api/bio-entity.ts
+++ src/data-sources/intermine/api/bio-entity.ts
@@ -3,6 +3,6 @@ import {intermineJoin} from '../intermine.server.js';
 export function bioEntityJoinFactory(model = 'BioEntity') {
   return [
     intermineJoin(`${model}.organism`, 'INNER'),
-    intermineJoin(`${model}.strain`, 'OUTER'),
+    intermineJoin(`${model}.strain`, 'INNER'),
   ];
 }
