f34a17f5c4f97a19215729d72044e84e180188f9
Matt Wiese <github@mattwie.se>
2026-01-13 17:37:33 -0500

    Re-enable INTERMINE_URI env var

--- src/index.ts
+++ src/index.ts
@@ -20,9 +20,7 @@ const server = new ApolloServer<ContextValue>({
 });
 
 const port = Number(process.env.PORT) || 4000;
-const intermineURI = 'https://mines.dev.lis.ncgr.org/minimine-genefunction/service';
-//  process.env.INTERMINE_URI ||
-//  'https://mines.dev.lis.ncgr.org/minimine/service';
+const intermineURI = process.env.INTERMINE_URI || 'https://mines.dev.lis.ncgr.org/minimine/service';
 const microservicesURI =
   process.env.MICROSERVICES_URI || 'https://services.lis.ncgr.org';
 const {cache} = server;
